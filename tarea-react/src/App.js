b40a4738e9fbca46d268ae1ee212f3d44a63e9de
Tudela <t93937687@gmail.com>
2025-06-15 20:51:04 -0400

    inicia

---
+++ tarea-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola, mundo!</h1>
+      <p>Este es mi primer despliegue en GitHub Pages 🚀</p>
+    </div>
+  );
+}
+
+export default App;
