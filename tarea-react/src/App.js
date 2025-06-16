178b10e684ad3d0cfddebdc2eebb6f2486207955
Tudela <t93937687@gmail.com>
2025-06-15 21:18:21 -0400

    agregabndo base en vite.confignts

--- tarea-react/src/App.js
+++ tarea-react/src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>¡Hola, mundo!</h1>
-      <p>Este es mi primer despliegue en GitHub Pages 🚀</p>
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
     </div>
   );
 }
